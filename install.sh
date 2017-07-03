8523496aa4b98ca3e2dd6f5f4381ba1add596430
Eric Lujan <eric@lujan.io>
2017-07-03 12:09:37 -0700

    Add customized zsh boilerplate

--- install.sh
+++ install.sh
@@ -34,12 +34,35 @@ if [[ ! -d "$HOMEDIR/.i3" ]] && [[ ! -L "$HOMEDIR/.i3" ]]; then
         mkdir "$HOMEDIR/.i3"
         ln -s "$CURRENTDIR/linux/i3/config" "$HOMEDIR/.i3/config"
     else
-        echo "SKIP install i3 configuration ($HOMEDIR/.i3/config already exists)"
+        echo "SKIP i3 configuration ($HOMEDIR/.i3/config already exists)"
     fi
 else
-    echo "SKIP install i3 configuration ($HOMEDIR/.i3 directory already present)"
+    echo "SKIP i3 configuration ($HOMEDIR/.i3 directory already present)"
+fi
+
+# oh-my-zsh install
+if [[ ! -d "$HOMEDIR/.oh-my-zsh" ]] && [[ ! -L "$HOMEDIR/.oh-my-zsh" ]]; then
+    echo "INSTALL oh-my-zsh"
+    ln -s "$CURRENTDIR/common/oh-my-zsh" "$HOMEDIR/.oh-my-zsh"
+    if [[ ! -e "$HOMEDIR/.zshrc" ]] && [[ ! -L "$HOMEDIR/.zshrc" ]]; then
+        echo "INSTALL oh-my-zsh zshrc"
+        cp "$CURRENTDIR/common/oh-my-zsh/templates/zshrc.zsh-template" "$HOMEDIR/.zshrc"
+    else
+        echo "SKIP oh-my-zsh zshrc ($HOMEDIR/.zshrc already present)"
+    fi
+else
+    echo "SKIP oh-my-zsh ($HOMEDIR/.oh-my-zsh already exists)"
+fi
+
+# Custom zsh stuff
+if [[ ! -e "$HOMEDIR/.zshrc.mine" ]] && [[ ! -L "$HOMEDIR/.zshrc.mine" ]]; then
+    echo "INSTALL custom zshrc"
+    ln -s "$CURRENTDIR/common/zshrc.mine" "$HOMEDIR/.zshrc.mine"
+    echo "HOOK custom zshrc"
+    echo "source $HOMEDIR/.zshrc.mine" >> "$HOMEDIR/.zshrc"
+else
+    echo "SKIP custom zshrc ($HOMEDIR/.zshrc.mine already present)"
 fi
 
 # Done!
 echo "Installation complete!"
-
