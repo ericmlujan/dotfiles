16af275f155f32cc249103dc8bf566168fcf393a
Eric Lujan <eric@lujan.io>
2017-07-03 02:36:35 -0700

    Add spacemacs to install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# An humble installer script for lujan's dotfiles
+
+set -euf -o pipefail
+
+# Global options
+HOMEDIR=$HOME
+
+# Emacs / Spacemacs install
+if [[ ! -d "$HOMEDIR/.emacs.d" ]] && [[ ! -L "$HOMEDIR/.emacs.d" ]]; then 
+    echo "INSTALL spacemacs"
+    ln -s spacemacs $HOMEDIR/.emacs.d
+else
+    echo "SKIP spacemacs ($HOMEDIR/.emacs.d already exists)"
+fi
+
+# Done!
+echo "Installation complete!"
+
