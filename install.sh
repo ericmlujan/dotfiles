e1f9986eb6d0fd655d0d92e4215a97b643dfec43
Eric Lujan <eric@lujan.io>
2017-07-03 02:58:45 -0700

    Add .spacemacs

--- install.sh
+++ install.sh
@@ -8,12 +8,23 @@ set -euf -o pipefail
 HOMEDIR=$HOME
 CURRENTDIR="$(pwd)"
 
+# Update git submodules
+echo "UPDATE git submodules"
+git submodule update --recursive
+
 # Emacs / Spacemacs install
 if [[ ! -d "$HOMEDIR/.emacs.d" ]] && [[ ! -L "$HOMEDIR/.emacs.d" ]]; then 
-    echo "INSTALL spacemacs"
+    echo "INSTALL spacemacs distribution"
     ln -s $CURRENTDIR/spacemacs $HOMEDIR/.emacs.d
 else
-    echo "SKIP spacemacs ($HOMEDIR/.emacs.d already exists)"
+    echo "SKIP spacemacs distribution ($HOMEDIR/.emacs.d already exists)"
+fi
+
+if [[ ! -e "$HOMEDIR/.spacemacs" ]] && [[ ! -L "$HOMEDIR/.spacemacs" ]]; then
+    echo "INSTALL spacemacs customizations"
+    ln -s $CURRENTDIR/emacs/dotspacemacs $HOMEDIR/.spacemacs
+else
+    echo "SKIP spacemacs customizations ($HOMEDIR/.spacemacs already exists)"
 fi
 
 # Done!
